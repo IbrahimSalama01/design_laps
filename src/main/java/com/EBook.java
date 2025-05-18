6630e0b61497d27044b5e80c139f61e77aa9229e
Ibrahim Salama <ibrahimsalama100@outlook.com>
2025-05-18 07:54:04 +0300

    Add access strategy interface and premium access implementation to EBook class

--- src/main/java/com/EBook.java
+++ src/main/java/com/EBook.java
@@ -1,8 +1,27 @@
 package com;
-public class EBook extends Book{
 
+interface AccessStrategy {
+    void access(User user);
+}
+
+class PremiumAccess implements AccessStrategy {
+    public void access(User user) {
+        System.out.println("Premium access granted.");
+    }
+}
+
+public class EBook extends Book{
+    private AccessStrategy accessStrategy;
     public EBook(String title) {
         super(title);
+    }
+
+    public void setAccessStrategy(AccessStrategy strategy) {
+        accessStrategy = strategy;
+    }
+
+    public void access(User user) {
+        accessStrategy.access(user);
     }
 
 }
